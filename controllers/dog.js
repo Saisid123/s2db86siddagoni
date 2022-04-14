d03496c4796e1df0d0612992b09617ab4d41852a
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-04-14 18:30:32 -0500

    Update dog.js

--- controllers/dog.js
+++ controllers/dog.js
@@ -1,8 +1,15 @@
 var dog = require('../models/dog'); 
  
 // List of all Costumes 
-exports.dog_list = function(req, res) { 
-    res.send('NOT IMPLEMENTED: dog list'); 
+exports.dog_list = async function(req, res) {
+    try{
+    thedogs = await hotel.find();
+    res.send(thedogs);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
 }; 
  
 // for a specific Costume. 
