ab5af2013b386f065ebdf2eea968edb181063bea
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-04-14 18:04:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ mongoose.connect(connectionString,
 {useNewUrlParser: true,
 useUnifiedTopology: true});
 
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+  console.log("Connection to DB succeeded")});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogRouter = require('./routes/dog');
