b341d5c479d3060e2aba01b984e7623cc2620204
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-04-14 21:07:49 -0500

    Update dog.js

--- controllers/dog.js
+++ controllers/dog.js
@@ -52,8 +52,23 @@ exports.dog_delete = function(req, res) {
 }; 
  
 // Handle Costume update form on PUT. 
-exports.dog_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: dog update PUT' + req.params.id); 
+exports.dog_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await dog.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.dog_breed)
+ toUpdate.dog_breed = req.body.dog_breed;
+ if(req.body.age) toUpdate.age = req.body.age;
+ if(req.body.name) toUpdate.name = req.body.name;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
 };
 
 // VIEWS
