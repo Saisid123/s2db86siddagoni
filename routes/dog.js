24883213655876593b0feb18a7d577c8a25dd21d
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-04-19 22:43:37 -0500

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -6,4 +6,5 @@ var router = express.Router();
 router.get('/', dog_controlers.dog_view_all_Page );
 router.get('/detail', dog_controlers.dog_view_one_Page);
 router.get('/create', dog_controlers.dog_create_Page);
+router.get('/update', dog_controlers.dog_update_Page);
 module.exports = router;
