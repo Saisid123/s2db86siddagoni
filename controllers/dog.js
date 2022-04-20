293d73256b2d2cba34bea333504e9536a28a8303
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-04-19 21:48:37 -0500

    Update dog.js

--- controllers/dog.js
+++ controllers/dog.js
@@ -91,3 +91,17 @@ exports.dog_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 }; 
+
+// Handle a show one view with id specified by query
+exports.dog_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await dog.findById( req.query.id)
+    res.render('dogdetail',
+   { title: 'dog Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
