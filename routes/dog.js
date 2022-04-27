7309773ae4afd75eb6250b596cfc05c0af2cb3f3
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-04-27 17:56:18 -0500

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -1,7 +1,17 @@
 var express = require('express');
 const dog_controlers= require('../controllers/dog');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+ 
 
 /* GET home page. */
 router.get('/', dog_controlers.dog_view_all_Page );
