74ce3c43285d57ce08b64ec030f5d1a06f1a3923
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-04-27 19:08:52 -0500

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -16,7 +16,7 @@ const secured = (req, res, next) => {
 /* GET home page. */
 router.get('/', dog_controlers.dog_view_all_Page );
 router.get('/detail', dog_controlers.dog_view_one_Page);
-router.get('/create', dog_controlers.dog_create_Page);
-router.get('/update', dog_controlers.dog_update_Page);
-router.get('/delete', dog_controlers.dog_delete_Page);
+router.get('/create',secured, dog_controlers.dog_create_Page);
+router.get('/update',secured, dog_controlers.dog_update_Page);
+router.get('/delete',secured, dog_controlers.dog_delete_Page);
 module.exports = router;
